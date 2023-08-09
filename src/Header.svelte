e303fc5afeae0d3718e7e1acb6fd8ed6bda5182d
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-08-09 03:46:57 +0200

    added hamburger menu for mobile

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,10 +1,34 @@
 <script>
+  import { Hamburger } from 'svelte-hamburgers';
   import Menu from './Menu.svelte';
+
+  let open;
 </script>
 
 <div class="header">
-  <img src="../logotyp-florescent.svg" alt="Florescent Logo" />
+  <div>
+    <div class="mobile-hamburger">
+      <Hamburger
+        --border-radius="0"
+        --layer-height="0.15rem"
+        --layer-spacing="0.5rem"
+        --layer-width="1.7rem"
+        --padding="0"
+        bind:open
+      />
+    </div>
+    <img
+      class="logo-desktop-tablet"
+      src="../logotyp-florescent.svg"
+      alt="Florescent Logo"
+    />
+  </div>
   <Menu />
+  <img
+    class="logo-mobile"
+    src="../logotyp-florescent.svg"
+    alt="Florescent Logo"
+  />
   <span class="material-symbols-outlined"> shopping_bag </span>
 </div>
 
@@ -18,14 +42,45 @@
     padding-right: 2rem;
   }
 
-  img {
-    width: 12rem;
-    height: auto;
-    padding-left: 0.4rem;
+  .mobile-hamburger {
+    display: none;
   }
 
+  .logo-desktop-tablet {
+    padding-left: 0.5rem;
+    width: 12rem;
+    height: auto;
+  }
+
+  .logo-mobile {
+    width: 12rem;
+    height: auto;
+  }
+
+  .logo-mobile {
+    display: none;
+  }
   .material-symbols-outlined {
     font-size: 2rem;
     text-align: right;
+  }
+
+  /* Media query for mobile screens */
+  @media (max-width: 768px) {
+    .header {
+      padding-left: 1rem;
+      padding-right: 1rem;
+    }
+    .logo-desktop-tablet {
+      display: none;
+    }
+
+    .logo-mobile {
+      display: block;
+    }
+    .mobile-hamburger {
+      display: block;
+      padding-left: 0.5rem;
+    }
   }
 </style>
