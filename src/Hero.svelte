e0ac50a7992393868128d584542c436fbdc935d9
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-08-10 02:25:13 +0200

    added hero component

---
+++ src/Hero.svelte
@@ -0,0 +1,35 @@
+<div class="hero-container">
+  <img class="iris" src="../iris.jpeg" alt="iris flower" />
+  <img class="hero" src="../hero.jpeg" alt="perfume bottles" />
+</div>
+
+<style>
+  .hero-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    overflow: hidden;
+  }
+
+  .iris {
+    max-width: 30%;
+    height: auto;
+  }
+
+  .hero {
+    max-width: 71.1%;
+    height: auto;
+  }
+
+  /* Media query for mobile screens */
+  @media (max-width: 768px) {
+    .iris {
+      display: none;
+    }
+
+    .hero {
+      max-width: 100%;
+    }
+  }
+</style>
