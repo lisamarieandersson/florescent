5ad730ce015c63c9d4b3a839b7b23c3936d82749
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-08-11 02:04:34 +0200

    added presentation component

---
+++ src/Presentation.svelte
@@ -0,0 +1,33 @@
+<div class="presentation-container">
+  <!-- <div>hello</div> -->
+  <div class="presentation-text">
+    <h2>Your New Ritual Reset, refresh and create space for the self.</h2>
+    <p>
+      The world around us is brimmed over with images, voices, and muddling
+      messages from media. Yet with the line between reality and fiction being
+      blurred, genuineness of inner self and calmness in voice should seep
+      deeper within. Stories of Florescent shall begin from starting simple
+      rituals in the midst of all the hustles and listening closer to inner
+      voice, not being swayed by messages from outside.
+    </p>
+  </div>
+</div>
+
+<style>
+  .presentation-container {
+    /* display: grid;
+    grid-template-columns: 1fr 1fr; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    overflow: hidden;
+    background-color: #f2efea;
+    padding: 4rem;
+    margin-top: 2.5rem;
+  }
+
+  .presentation-text {
+    text-align: center;
+  }
+</style>
