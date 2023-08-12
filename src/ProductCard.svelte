96944c2c5f88c8187bc880a62d9ff59fe05696cc
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-08-12 23:48:17 +0200

    added product card component

---
+++ src/ProductCard.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let product: {
+    name: string;
+    image: string;
+    description: string;
+  };
+</script>
+
+<div class="product-card">
+  <img src={product.image} alt={product.name} />
+  <h3>{product.name}</h3>
+  <p>{product.description}</p>
+  <button>Add to Cart</button>
+</div>
+
+<style>
+  .product-card {
+    text-align: center;
+  }
+
+  img {
+    max-width: 70%;
+    height: auto;
+  }
+
+  button {
+    border-radius: 0;
+    font-family: 'Fraunces', serif;
+    font-weight: 300;
+    background: #ffffff;
+    border: solid 0.025rem #000000;
+    padding: 1rem;
+    text-transform: uppercase;
+    width: 70%;
+  }
+</style>
