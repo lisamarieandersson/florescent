415ef4e7e5f2f8409aec3636442504850c26a900
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-08-15 22:44:59 +0200

    edited comments

--- src/Hero.svelte
+++ src/Hero.svelte
@@ -25,42 +25,42 @@
     object-fit: cover;
   }
 
-  /* Media query for really large screens */
+  /* Media query for really large desktop screens */
   @media (max-width: 3840px) {
     .hero-container {
       height: 102rem;
     }
   }
 
-  /* Media query for large screens */
+  /* Media query for large desktop screens */
   @media (max-width: 2560px) {
     .hero-container {
       height: 64rem;
     }
   }
 
-  /* Media query for standard screens */
+  /* Media query for standard desktop screens */
   @media (max-width: 1920px) {
     .hero-container {
       height: 48rem;
     }
   }
 
-  /* Media query for standard screens */
+  /* Media query for standard desktop screens */
   @media (max-width: 1440px) {
     .hero-container {
       height: 36rem;
     }
   }
 
-  /* Media query for standard screens */
+  /* Media query for standard desktop screens */
   @media (max-width: 1280px) {
     .hero-container {
       height: 33rem;
     }
   }
 
-  /* Media query for mobile and tablet screens */
+  /* Media query for tablet and mobile screens */
   @media (max-width: 768px) {
     .hero-container {
       height: 30rem;
